329bbb085ffe1b28aa4a583abf339bdb5c3f81f6
Petersingh Anburaj <panburaj@redhat.com>
2018-02-20 18:53:11 +0530

    Added Class for parser

--- virshdumpparser/parser.py
+++ virshdumpparser/parser.py
@@ -5,6 +5,43 @@ import xml.etree.ElementTree as ET
 from terminaltables import AsciiTable
 
 
+class VirshXMLParser:
+
+    def __init__(self, xmlfile=None):
+        self.root = None
+        if xmlfile:
+            tree = ET.parse(xmlfile)
+            self.root = tree.getroot()
+
+    def get_attrib(self, attr, **args):
+        element = self.get_elements(**args)
+        if element is None:
+            return 'None'
+        else:
+            return element.get(attr)
+
+    def get_elements(self, path=None, tag=None, attrib=None, value=None, ns={}, listout=False):
+        elements = [self.root]
+        search = '.' if not path else path
+        if tag:
+            search = search + tag
+        if attrib and value:
+            search = "%s[@%s='%s']" %(search, attrib, value)
+        elements = self.root.findall(search, ns)
+        if listout:
+            return elements
+        if elements:
+            return elements[0]
+        return None
+
+    def get_text(self, **args):
+        element = self.get_elements(**args)
+        if element is None:
+            return 'None'
+        else:
+            return element.text
+
+
 def main():
 # Main function which will be called
 # to parse the list of xml files.
@@ -12,65 +49,70 @@ def main():
     for xmlfile in sys.argv[1:]:
 
         # create element tree object
-        tree = ET.parse(xmlfile)
+        #tree = ET.parse(xmlfile)
+        # get root element
+        #root = tree.getroot()
+        VXMLP = VirshXMLParser(xmlfile)
+
         table_data = []
         table_data.append(['Field', 'Value'])
 
-        # get root element
-        root = tree.getroot()
 
+        table_data.append(['Name', VXMLP.get_text(tag='name')])
 
-        table_data.append(['Name', get_text(root, tag='name')])
+        table_data.append(['Domian Id', VXMLP.get_attrib('id')])
 
-        table_data.append(['Domian Id', get_attrib(root, 'id')])
-
-        table_data.append(['Instance UUID', get_text(root, tag='uuid')])
+        table_data.append(['Instance UUID', VXMLP.get_text(tag='uuid')])
         
 
         ns = {'nova': "http://openstack.org/xmlns/libvirt/nova/1.0"}
-        name = get_text(root, path='.//', tag='nova:name', ns=ns)
+        name = VXMLP.get_text(path='.//', tag='nova:name', ns=ns)
         table_data.append(['Instance Name', name])
         
-        flavor_name = get_attrib(root, 'name', path='.//', tag='nova:flavor', ns=ns)
-        flavor_disk = get_text(root, path='.//', tag='nova:memory', ns=ns)
-        flavor_vcpu = get_text(root, path='.//', tag='nova:vcpus', ns=ns)
-        flavor_memory = get_text(root, path='.//', tag='nova:memory', ns=ns)
+        flavor_name = VXMLP.get_attrib('name', path='.//', tag='nova:flavor', ns=ns)
+        flavor_disk = VXMLP.get_text(path='.//', tag='nova:memory', ns=ns)
+        flavor_vcpu = VXMLP.get_text(path='.//', tag='nova:vcpus', ns=ns)
+        flavor_memory = VXMLP.get_text(path='.//', tag='nova:memory', ns=ns)
         flavor = "%s (%s  MB, %s VCPUs, %s GB)" %(flavor_name, flavor_memory, flavor_vcpu, flavor_disk)
         table_data.append(['Flavor', flavor])
         
-        image = get_attrib(root, 'uuid', path='.//', tag='nova:root', ns=ns)
+        image = VXMLP.get_attrib('uuid', path='.//', tag='nova:root', ns=ns)
         table_data.append(['Image ID', image])
 
         
-        interfaces = get_elements(root, path='./devices/', tag='interface', listout=True)
+        interfaces = VXMLP.get_elements(path='./devices/', tag='interface', listout=True)
         table_data.append(['No of Interfaces', len(interfaces)])
 
+        InterfaceVXMLP = VirshXMLParser()
         for interface in interfaces:
-            itype = get_attrib(interface, 'type')
-            mac = get_attrib(interface, 'address', tag='mac')
-            driver = get_attrib(interface, 'name', tag='driver')
-            atype = get_attrib(interface, 'type', tag='address')
-            domain = get_attrib(interface, 'domain', tag='address')
-            bus = get_attrib(interface, 'bus', tag='address')
-            slot = get_attrib(interface, 'slot', tag='address')
+            InterfaceVXMLP.root = interface
+            itype = InterfaceVXMLP.get_attrib('type')
+            mac = InterfaceVXMLP.get_attrib('address', tag='mac')
+            driver = InterfaceVXMLP.get_attrib('name', tag='driver')
+            atype = InterfaceVXMLP.get_attrib('type', tag='address')
+            domain = InterfaceVXMLP.get_attrib('domain', tag='address')
+            bus = InterfaceVXMLP.get_attrib('bus', tag='address')
+            slot = InterfaceVXMLP.get_attrib('slot', tag='address')
             iname = "Interface (%s)" %(mac)
             idetails = "Interface Type: %s\nMAC: %s\nDriver: %s\nAddress Type: %s\nDomain: %s\nBus: %s\nSlot: %s" %(itype, mac, driver, atype, domain, bus, slot)
             table_data.append([iname, idetails])
 
 
-        disks = get_elements(root, path='./devices/', tag='disk', listout=True)
+        disks = VXMLP.get_elements(path='./devices/', tag='disk', listout=True)
         table_data.append(['No of Disks', len(disks)])
 
+        DiskVXMLP = VirshXMLParser()
         for disk in disks:
-            dtype = get_attrib(disk, 'type')
-            drtype = get_attrib(disk, 'type', tag='driver')
-            target = get_attrib(disk, 'dev', tag='target')
-            bus = get_attrib(disk, 'bus', tag='target')
-            serial = get_text(disk, tag='serial')
+            DiskVXMLP.root = disk
+            dtype = DiskVXMLP.get_attrib('type')
+            drtype = DiskVXMLP.get_attrib('type', tag='driver')
+            target = DiskVXMLP.get_attrib('dev', tag='target')
+            bus = DiskVXMLP.get_attrib('bus', tag='target')
+            serial = DiskVXMLP.get_text(tag='serial')
             dname = "Disk (%s)" %(target)
             ddetails = "Disk Type: %s\nDriver Type: %s\nDevice Path: %s\nBus: %s\nSerial No: %s" %(dtype, drtype, target, bus, serial)
 
-            source = get_elements(disk, tag='source')
+            source = DiskVXMLP.get_elements(tag='source')
             source_attrib = ''
             for attrib in source.keys():
                 source_attrib += "   %s: %s\n" %(attrib, source.get(attrib))
@@ -83,33 +125,6 @@ def main():
         print(table.table)
 
 
-def get_attrib(element, attr, **args):
-    element = get_elements(element, **args)
-    if element is None:
-        return 'None'
-    else:
-        return element.get(attr)
-
-def get_elements(element, path=None, tag=None, attrib=None, value=None, ns={}, listout=False):
-    elements = [element]
-    search = '.' if not path else path
-    if tag:
-        search = search + tag
-    if attrib and value:
-        search = "%s[@%s='%s']" %(search, attrib, value)
-    elements = element.findall(search, ns)
-    if listout:
-        return elements
-    if elements:
-        return elements[0]
-    return None
-
-def get_text(element, **args):
-    element = get_elements(element, **args)
-    if element is None:
-        return 'None'
-    else:
-        return element.text
 
 if __name__ == "__main__":
  
